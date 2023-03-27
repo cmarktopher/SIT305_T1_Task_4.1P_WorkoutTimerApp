d5778ca318a10944dc92b89bd5f772f3e7611b78
chris-marktopher <cmarktopher@gmail.com>
2023-03-27 13:55:19 +1100

    Added in a custom progress bar

--- app/src/main/java/com/application/workouttimer/MainActivity.java
+++ app/src/main/java/com/application/workouttimer/MainActivity.java
@@ -3,12 +3,26 @@ package com.application.workouttimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // UI Elements
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.durationProgressBar);
+        progressBar.setIndeterminate(false);
+        progressBar.setMax(100);
+        progressBar.setProgress(50);
+
+
+
     }
 }
